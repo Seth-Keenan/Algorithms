d92bc342f34a1404186bd36e652b59eaf4520082
Seth Keenan <seth.j.keenan@gmail.com>
2023-04-29 15:37:53 -0500

    Added Features

--- C/treeSort/treeSort.h
+++ C/treeSort/treeSort.h
@@ -3,8 +3,14 @@
 
 typedef struct tree tree;
 
+//This function is used to insert data in the form of integers into the tree structure.
+//This is the time complexity of O(log(n)).
 void insert(tree **, int);
 
+//This function is simply called to print the tree structure.
+//This is the time complexity of O(log(n)).
 void printTree(tree *);
 
+//This function is used to free the tree structure back to memory
+//This function is the time complexity of O(log(n)).
 void freeTree(tree *);
